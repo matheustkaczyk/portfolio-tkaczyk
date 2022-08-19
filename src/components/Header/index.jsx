3821ada0016579cf2ee132bf0492fdb8c0343906
matheustkaczyk <matheus111904@hotmail.com>
2022-08-19 10:19:07 -0300

    feat: instalando sass, corrigindo imports, criando nav no header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,6 +5,13 @@ const Header = () => {
     <header>
       <h2>Tkaczyk</h2>
       <GiHamburgerMenu />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
   )
 }
