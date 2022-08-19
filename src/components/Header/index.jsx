ae505b50621b7eebd193055297fdfff55d1f93df
matheustkaczyk <matheus111904@hotmail.com>
2022-08-19 10:15:26 -0300

    feat: componentizando header, instalando react-icons

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Tkaczyk</h1>
+      <GiHamburgerMenu />
+    </header>
+  )
+}
+
+export default Header;
