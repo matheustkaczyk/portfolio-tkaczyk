dc33fb9236e54957bce14cf57837e011c13489b0
matheustkaczyk <matheus111904@hotmail.com>
2022-08-19 10:25:51 -0300

    feat: criado menu hamburger

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,20 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Header = () => {
+  const handleHamburger = () => {
+    const nav = document.getElementsByTagName('nav')[0];
+    nav.classList.toggle('active');
+  }
+
   return(
     <header>
-      <h2>Tkaczyk</h2>
-      <GiHamburgerMenu />
+      <div>
+        <h2>Tkaczyk</h2>
+        <GiHamburgerMenu onClick={() => handleHamburger()} />
+      </div>
       <nav>
+        <AiOutlineClose onClick={() => handleHamburger()} />
         <ul>
           <li>Home</li>
           <li>About</li>
