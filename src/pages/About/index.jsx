d2716d55eb9e5378ff1375ac2fe68e95bcdd3ee8
matheustkaczyk <matheus111904@hotmail.com>
2022-08-28 11:23:37 -0300

    fix: corrigindo instalando do plug vite, renderizando svg na tela

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,10 @@
+import { ReactComponent as ProgrammingSvg } from '../../assets/images/programming.svg';
+
 const About = () => {
   return(
     <section className="about-wrapper" id="about">
       <div className="about">
+        <ProgrammingSvg />
         <h2>I'm very passionate about science, technology and programming.</h2>
       </div>
     </section>
