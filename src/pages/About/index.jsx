bf89972c4cbcea28ada18a2bed87152a2737edfd
matheustkaczyk <matheus111904@hotmail.com>
2022-08-28 19:42:07 -0300

    feat: estilizacao about concluida

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,12 +1,20 @@
 import { ReactComponent as ProgrammingSvg } from '../../assets/images/programming.svg';
+import { ReactComponent as KnowledgeSvg } from '../../assets/images/knowledge.svg';
 
 const About = () => {
   return(
     <section className="about-wrapper" id="about">
       <div className="about">
-        <ProgrammingSvg />
-        <h4>I'm very passionate about science, technology and programming.</h4>
-        <hr />
+        <div className='about-heading'>
+          <ProgrammingSvg />
+          <h4>I'm very passionate about science, technology and programming.</h4>
+          <hr />
+        </div>
+        <div className='about-info'>
+          <p>I recently finished the web development course at <a className='trybe' href='https://www.betrybe.com/' target={'__blank'}>Trybe</a>, where for the last year i've been learning and 
+          practicing all the technologies about the fullstack web development. Right now, i'm studying Computer Science at <a className='estacio' href='https://www.estacio.br' target={'__blank'}>Estácio</a></p>
+        </div>
+        <KnowledgeSvg className="knowledge-svg" />
       </div>
     </section>
   )
