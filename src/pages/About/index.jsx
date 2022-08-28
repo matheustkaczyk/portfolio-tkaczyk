a62935efb295642c8c8e8521a62649c11c1f5967
matheustkaczyk <matheus111904@hotmail.com>
2022-08-28 11:06:14 -0300

    fix: corrigindo estrutura da page about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,9 @@
 const About = () => {
   return(
     <section className="about-wrapper" id="about">
-
+      <div className="about">
+        <h2>I'm very passionate about science, technology and programming.</h2>
+      </div>
     </section>
   )
 }
