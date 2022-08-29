1a939311272419df1914b606883157802af29f73
matheustkaczyk <matheus111904@hotmail.com>
2022-08-29 11:06:52 -0300

    fix: corrigindo typo no about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -12,7 +12,7 @@ const About = () => {
         </div>
         <div className='about-info'>
           <p>I recently finished the web development course at <a className='trybe' href='https://www.betrybe.com/' target={'__blank'}>Trybe</a>, where for the last year i've been learning and 
-          practicing all the technologies about the fullstack web development. Right now, i'm studying Computer Science at <a className='estacio' href='https://www.estacio.br' target={'__blank'}>Estácio</a></p>
+          practicing all the technologies about fullstack web development. Right now, i'm studying Computer Science at <a className='estacio' href='https://www.estacio.br' target={'__blank'}>Estácio</a></p>
         </div>
         <KnowledgeSvg className="knowledge-svg" />
       </div>
