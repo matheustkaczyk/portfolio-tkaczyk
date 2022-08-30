7d6b97cbc7920cb5db477ed0790d17062588cf92
matheustkaczyk <matheus111904@hotmail.com>
2022-08-30 15:13:06 -0300

    feat: organizando informações dos projetos

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,7 +1,26 @@
 const Projects = () => {
+  const projects = [
+    {
+      name: 'Trivia',
+      url: 'https://vercel.com/matheustkaczyk/project-trivia/7peEbgPebbWuH9pADxtvRSk5ExCK'
+    },
+    {
+      name: 'Splitter',
+      url: 'https://vercel.com/matheustkaczyk/splitter-challenge/GDLnn2XKKMHTEFBsQDzhLQqXGWoP'
+    },
+    {
+      name: 'Easybank',
+      url: 'https://vercel.com/matheustkaczyk/easybank/FuYb9vJY7HvSQXHdNb1BNT6r1odX'
+    },
+    {
+      name: 'QrCode Generator',
+      url: 'https://qrcode-gen-tka.netlify.app/'
+    }
+  ];
+
   return(
     <section className="projects">
-      
+
     </section>
   )
 }
