6452a8bb27f9f62c116803bc6d735fc83c3ee7c3
matheustkaczyk <matheus111904@hotmail.com>
2022-10-20 10:56:22 -0300

    refact: alterando a forma dos cards projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -9,38 +9,52 @@ const Projects = () => {
       name: 'Easybank',
       url: 'https://easybank-bsk1zeve9-matheustkaczyk.vercel.app/',
       image: easybank,
+      tags: ['HTML', 'SCSS', 'JavaScript', 'React'],
     },
     {
       name: 'Trivia',
       url: 'https://project-trivia-4beq4455q-matheustkaczyk.vercel.app/',
       image: trivia,
+      tags: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
     },
     {
       name: 'Splitter',
       url: 'https://splitter-challenge-l9x093db2-matheustkaczyk.vercel.app/',
       image: splitter,
+      tags: ['HTML', 'CSS', 'JavaScript', 'React'],
     },
     {
       name: 'QrCode',
       url: 'https://qrcode-gen-tka.netlify.app/',
       image: qrcode,
+      tags: ['HTML', 'CSS', 'JavaScript', 'React'],
     }
   ];
 
-  return(
+  return (
     <section className="projects-wrapper">
       <h2>Projects</h2>
       <p>Here's a list of a few projects that i made.</p>
       <div className="projects">
         {
           projects.map(project => (
-              <div className='project-card' key={project.name}>
-                <div className='project-image'>
-                  <a href={project.url} target="_blank" rel="noopener noreferrer">
-                    <img src={project.image} />
-                  </a>
+            <div className='project-card' key={project.name}>
+              <div className='project-image'>
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <img src={project.image} />
+                </a>
+              </div>
+              <div className='project-info'>
+                <h3>{project.name}</h3>
+                <div className='project-tags'>
+                  {
+                    project.tags.map(tag => (
+                      <span key={tag}>{tag}</span>
+                    ))
+                  }
                 </div>
               </div>
+            </div>
           ))
         }
       </div>
